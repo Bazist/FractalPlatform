2ae6fa36d8f39390602967c1f0f5b270a735593b
Bazist <booben90@gmail.com>
2022-06-19 00:24:30 +0300

    to do list

---
+++ FractalPlatform.Examples/Applications/ToDo/ToDoApplication.cs
@@ -0,0 +1,27 @@
+﻿using BigDoc.Client;
+using BigDoc.Client.UI;
+using BigDoc.Database.Engine;
+using System;
+
+namespace FractalPlatform.Examples
+{
+    public class ToDoApplication : BaseApplication
+    {
+        public ToDoApplication(Guid sessionId,
+                               BigDocInstance instance, 
+                               IFormFactory formFactory) : base(sessionId,
+                                                                    instance,
+                                                                    formFactory,
+                                                                    "ToDo")
+        {
+        }
+
+        public override void OnStart(Context context)
+        {
+            Client.SetDefaultCollection("ToDo")
+                  .GetDoc(Constants.FIRST_DOC_ID)
+                  .WantModifyExistingDocuments()
+                  .OpenForm();
+        }
+    }
+}
