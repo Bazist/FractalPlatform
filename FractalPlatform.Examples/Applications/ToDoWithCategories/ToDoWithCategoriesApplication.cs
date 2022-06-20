9d4452f4738d8018e84fcaa32f671351b5d70f03
Viacheslav Makoveichuk <vmakoveichuk@deloitte.com>
2022-06-20 21:23:20 +0300

    fix

---
+++ FractalPlatform.Examples/Applications/ToDoWithCategories/ToDoWithCategoriesApplication.cs
@@ -0,0 +1,25 @@
+﻿using BigDoc.Client;
+using BigDoc.Client.UI;
+using BigDoc.Database.Engine;
+using System;
+
+namespace FractalPlatform.Examples
+{
+    public class ToDoWithCategoriesApplication : BaseApplication
+    {
+        public ToDoWithCategoriesApplication(string workingFolder,
+                               IFormFactory formFactory) : base(workingFolder,
+                                                                "ToDoWithCategories",
+                                                                formFactory)
+        {
+        }
+
+        public override void OnStart(Context context)
+        {
+            Client.SetDefaultCollection("ToDoColl")
+                  .GetDoc(Constants.FIRST_DOC_ID)
+                  .WantModifyExistingDocuments()
+                  .OpenForm();
+        }
+    }
+}
