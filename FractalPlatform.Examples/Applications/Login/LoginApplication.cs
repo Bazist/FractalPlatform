c2fd86c51bad3035d4573a34682521f7746de2ec
Viacheslav Makoveichuk <vmakoveichuk@deloitte.com>
2022-06-21 23:51:58 +0300

    fixed

---
+++ FractalPlatform.Examples/Applications/Login/LoginApplication.cs
@@ -0,0 +1,25 @@
+﻿using BigDoc.Client;
+using BigDoc.Database.Engine;
+
+namespace FractalPlatform.Examples.Applications.Login
+{
+    public class LoginApplication : DashboardApplication
+    {
+        public LoginApplication(string workingFolder,
+                               IFormFactory formFactory) : base(workingFolder,
+                                                                "Login",
+                                                                formFactory)
+        {
+        }
+
+        public override void OnStart(Context context)
+        {
+            Login();
+        }
+
+        protected override void OnLogin(FormResult result)
+        {
+            MessageBox("You logged in as: " + UserContext.User.Name);
+        }
+    }
+}
