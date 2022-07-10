87dda4469d5492634f9adebbfce80c1ccbbe3051
Viacheslav Makoveichuk <vmakoveichuk@deloitte.com>
2022-07-10 12:17:58 +0300

    add new test app

---
+++ FractalPlatform.Examples/Applications/ToDo2/ToDo2Application.cs
@@ -0,0 +1,27 @@
+﻿using BigDoc.Client;
+using BigDoc.Client.UI;
+using BigDoc.Database.Engine;
+using System;
+
+namespace FractalPlatform.Examples.Applications.ToDo2
+{
+    public class ToDo2Application : BaseApplication
+    {
+        public ToDo2Application(Guid sessionId,
+                                        BigDocInstance instance,
+                                        IFormFactory formFactory) : base(sessionId,
+                                                                        instance,
+                                                                        formFactory,
+                                                                        "ToDo2")
+        {
+        }
+
+        public override void OnStart(Context context)
+        {
+            Client.SetDefaultCollection("Categories")
+                  .GetAll()
+                  .WantModifyExistingDocuments()
+                  .OpenForm();
+        }
+    }
+}
