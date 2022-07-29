cabf876e0e5f89c608ae2f4d9cf07df6205aa83e
Bazist <booben90@gmail.com>
2022-07-29 22:04:23 +0300

    add new proj

---
+++ FractalPlatform.Examples/Applications/ManagePasswords/ManagePasswordsApplication.cs
@@ -0,0 +1,27 @@
+﻿using BigDoc.Client;
+using BigDoc.Client.UI;
+using BigDoc.Database.Engine;
+using System;
+
+namespace FractalPlatform.Examples.Applications.ToDo
+{
+    public class ManagePasswordsApplication : BaseApplication
+    {
+        public ManagePasswordsApplication(Guid sessionId,
+                               BigDocInstance instance,
+                               IFormFactory formFactory) : base(sessionId,
+                                                               instance,
+                                                               formFactory,
+                                                               "ManagePasswords")
+        {
+        }
+
+        public override void OnStart(Context context)
+        {
+            Client.SetDefaultCollection("ManagePasswordsColl")
+                  .GetFirstDoc()
+                  .WantModifyExistingDocuments()
+                  .OpenForm();
+        }
+    }
+}
